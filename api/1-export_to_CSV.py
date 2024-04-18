bf24b9fe40e4969908b5817636a98ec58bc71dea
NidiMeliodas <nidigashan@gmail.com>
2024-04-18 12:06:00 +0200

    fixing task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,39 +3,34 @@
 Task 1 ------
 """
 
-import json
+
+import csv
 import requests
 import sys
-import csv
 
 
 def api(user_id):
-    user_link = 'https://jsonplaceholder.typicode.com/users' + user_id
-    todos_link = "https://jsonplaceholder.typicode.com/todos"
+    """
+    YES sirrr
+    """
+    user_link = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    todos_link = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
 
-    answer = requests.get(user_link)
+    user_name = user_link.json()['username']
 
-    if (answer.ok):
-        jData = json.loads(answer.content)
-        EMPLOYEE_NAME = jData["name"]
-    else:
-        answer.raise_for_status
-
-    query = {'userId': user_id}
-
-    answer = requests.get(todos_link, params=query)
-    if (answer.ok):
-        jData = json.loads(answer.content)
-
-        with open("{}.csv".format(user_id), "w", encoding='utf-8') as f:
-            csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-            for task in jData:
-                csv_writer.writerow([user_id, EMPLOYEE_NAME,
-                                    task.get("completed"),
-                                    task.get("title")])
-    else:
-        answer.raise_for_status()
+    for todo in todos_link.json():
+        task = todo['title']
+        task_status = todo['completed']
+        with open(f'{user_id}.csv', mode='a', newline='') as file:
+            csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            csv_writer.writerow([user_id, user_name, task_status, task])
 
 
-if __name__ == "__main__":
-    api(sys.argv[1])
+if __name__ == '__main__':
+    try:
+        user_id = sys.argv[1]
+        api(user_id)
+    except Exception as e:
+        pass
